804d499f44544a12033b6943c66155c00625c012
Isaac <Isaac@DESKTOP-NRALPAH>
2018-10-15 16:50:39 -0600

    Stack and Queue

---
+++ TheActualDataStructures/Controllers/QueueController.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TheActualDataStructures.Controllers
+{
+    public class QueueController : Controller
+    {
+
+        static Queue<string> myQ = new Queue<string>();
+
+        // GET: Queue
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult AddOne()
+        {
+            myQ.Enqueue("New Entry " + (myQ.Count + 1));
+            ViewBag.QueueStatus = "New Entry " +  myQ.Count + " has been added!";
+            return View("Index");
+        }
+
+        public ActionResult AddLots()
+        {
+            myQ.Clear();
+
+            for (int i = 0; i < 2000; i++)
+            {
+                myQ.Enqueue("New Entry " + (myQ.Count + 1));
+                ViewBag.QueueStatus = "Queue now contains " + myQ.Count + " items";
+            }
+            return View("Index");
+        }
+
+        public ActionResult Display()
+        {
+
+            ViewBag.theQueue = myQ;
+
+            return View("Index");
+        }
+
+        public ActionResult Delete()
+        {
+            if (myQ.Count == 0)
+            { ViewBag.QueueStatus = "There are no items in the Queue"; }
+            else
+            {
+                myQ.Dequeue();
+                ViewBag.QueueStatus = "Deleted item # " + (myQ.Count + 1);
+
+            }
+            return View("Index");
+        }
+
+        public ActionResult Clear()
+        {
+            myQ.Clear();
+            ViewBag.QueueStatus = "Queue now contains " + myQ.Count + " items";
+
+            return View("Index");
+        }
+
+        public ActionResult Search()
+        {
+            System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
+
+            sw.Start();
+
+            if (myQ.Contains("New Entry 20"))
+            {
+                ViewBag.QueueStatus = "Found";
+            }
+            else
+            {
+                ViewBag.QueueStatus = "Not Found"; ;
+            }
+
+            sw.Stop();
+
+            TimeSpan ts = sw.Elapsed;
+
+            ViewBag.QueueStatus += " in " + ts + " seconds.";
+
+
+
+
+
+            return View("Index");
+        }
+
+    }
+}
---
+++ TheActualDataStructures/Controllers/StackController.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TheActualDataStructures.Controllers
+{
+    public class StackController : Controller
+    {
+
+        static Stack <string>myStack = new Stack<string>();
+
+        // GET: Stack
+        public ActionResult Index()
+        {
+
+
+            return View();
+        }
+
+        public ActionResult AddOne()
+        {
+            myStack.Push("New Entry " +(myStack.Count + 1));
+            ViewBag.StackStatus = myStack.Peek() + " has been added!";
+            return View("Index");
+        }
+
+        public ActionResult AddLots()
+        {
+            myStack.Clear();
+
+            for (int i = 0; i < 2000; i++)
+            {
+                myStack.Push("New Entry " + (myStack.Count + 1));
+                ViewBag.StackStatus = "Stack now contains " + myStack.Count + " items";
+            }
+            return View("Index");
+        }
+
+        public ActionResult Display()
+        {
+           
+                ViewBag.TheStack = myStack;
+            
+            return View("Index");
+        }
+
+        public ActionResult Delete()
+        {
+            if (myStack.Count == 0)
+            { ViewBag.StackStatus = "There are no items in the stack"; }
+            else
+            {
+                myStack.Pop();
+                ViewBag.StackStatus = "Deleted item # " + ( myStack.Count + 1 );
+
+            }
+            return View("Index");
+        }
+
+        public ActionResult Clear()
+        {
+            myStack.Clear();
+            ViewBag.StackStatus = "Stack now contains " + myStack.Count + " items";
+
+            return View("Index");
+        }
+
+        public ActionResult Search()
+        {
+            System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
+
+            sw.Start();
+
+                    if (myStack.Contains("New Entry 20"))
+                    {
+                        ViewBag.StackStatus = "Found";
+                    }
+                    else
+                    {
+                        ViewBag.StackStatus = "Not Found"; ;
+                    }
+
+            sw.Stop();
+
+            TimeSpan ts = sw.Elapsed;
+
+            ViewBag.StackStatus += " in " + ts + " seconds.";
+            
+            
+
+
+
+            return View("Index");
+        }
+
+    }
+}
